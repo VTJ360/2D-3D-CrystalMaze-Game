34e8b50124a5c21a3d26b99f59f7d5f406581fd6
VTJ360 <vtjensen@outlook.com>
2017-06-10 17:48:26 +0100

    Add files via upload

---
+++ Launcher.java
@@ -0,0 +1,10 @@
+package dev.Vibb360.CrystalMaze;
+
+import dev.Vibb360.CrystalMaze.display.*;
+
+public class Launcher {
+	
+	public static void main(String[] args){
+		new Display("Title!", 300, 300);
+	}
+}
