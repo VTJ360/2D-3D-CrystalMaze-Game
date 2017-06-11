525513a0dccaabccb68a93afed64016e7522a88c
VTJ360 <vtjensen@outlook.com>
2017-06-11 11:02:50 +0100

    Add files via upload

---
+++ CrystalMaze2D/Display.java
@@ -0,0 +1,28 @@
+package dev.Vibb360.CrystalMaze.display;
+
+import javax.swing.JFrame;
+
+public class Display {
+	
+	private JFrame frame;
+	
+	private String title;
+	private int width, height;
+	
+	public Display(String title, int width, int height){
+		this.title = title;
+		this.height = height;
+		this.width = width;
+		
+		creatDisplay();
+	}
+	
+	private void creatDisplay(){
+		frame = new JFrame(title);
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+}
